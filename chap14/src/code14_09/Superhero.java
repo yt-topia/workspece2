6c7809da66f0ace1b4bc5c00987b397b867d4c76
yt-topia <oks.v5.ssyth@gmail.com>
2022-09-05 14:21:09 +0900

    コード内容の修正を行った

--- chap14/src/code14_09/Superhero.java
+++ chap14/src/code14_09/Superhero.java
@@ -4,19 +4,19 @@ public class Superhero extends Hero {
 //	String name = "ishida";
 //	String ss;
 //	int ii;
-	
+
 	public boolean equals(Object o) {
 		if(this == o) {
 			return true;
 		}
-/*
+
 		if(o instanceof Hero) {
 			Hero h = (Hero)o;
 			if(this.equals(h)) {
 				return true;
 			}
 		}
-*/
+
 		return false;
 	}
 }
